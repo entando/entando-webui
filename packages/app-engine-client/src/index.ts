b8bb2b81a8a9bcf86f44144e28627b70a687768e
Filipe Leandro <ffleandro>
2022-02-01 19:40:09 -0300

    Cleaning up and preparing for publishing version

--- packages/app-engine-client/src/index.ts
+++ packages/app-engine-client/src/index.ts
@@ -1,24 +1,6 @@
-export {
-  Entando6CorePageTemplateDataSource,
-  Entando6CoreFragmentSource,
-  Entando6CorePageConfigurationSource,
-  Entando6CorePageSettingsDataSource,
-  Entando6UserAuthoritiesDataSource,
-  Entando6CoreLanguagesDataSource,
-} from './core/core';
-
-export {
-  Entando6CMSContentsDataSource,
-  Entando6CMSContentDataSource,
-} from './core/cms';
-
-export {
-  Entando6PortalUIUrlDataSource,
-} from './core/portalui';
-
-export {
-  TokenCache,
-  fetchKeycloakToken,
-  getServerKeycloakToken,
-  refreshClientToken,
-} from './keycloak';
+export * from './core/core';
+export * from './core/cms';
+export * from './core/portalui';
+export * from './core/pages';
+export * from './keycloak';
+export * from './types';
