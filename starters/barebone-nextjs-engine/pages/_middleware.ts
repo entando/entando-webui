05e9c584c9b63d2c0b27c6d212b9359c68995b2f
Filipe Leandro <ffleandro>
2022-02-25 20:48:58 -0300

    Added middleware to support basepath configuration

--- starters/barebone-nextjs-engine/pages/_middleware.ts
+++ starters/barebone-nextjs-engine/pages/_middleware.ts
@@ -2,15 +2,18 @@ import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 import path from 'path';
 
 const PORTALUI_REWRITES = [
-  '/entando-de-app/cmsresources/',
-  '/entando-de-app/resources/',
-  '/entando-de-app/favicon.',
-  '/entando-de-app/keycloak.json',
-  '/entando-de-app/api/',
+  '/cmsresources/',
+  '/resources/',
+  '/favicon.',
+  '/keycloak.json',
+  '/api/',
 ];
 
+const PORTALUI_BASEPATH = new URL(`${process.env.PORTALUI_ADDR}`).pathname.replace(/\/$/, '');
+const BASEPATH = `${process.env.BASEPATH}`.replace(/\/$/, '');
+
 const shouldRewriteToPortalUi = (pathname: string): boolean => {
-  return PORTALUI_REWRITES.filter(p => pathname.includes(p)).length != 0;
+  return PORTALUI_REWRITES.filter(p => pathname.includes(`${PORTALUI_BASEPATH}${p}`)).length != 0;
 };
 
 const rewriteToPortalUi = (pathname: string): NextResponse => {
@@ -21,12 +24,23 @@ const rewriteToPortalUi = (pathname: string): NextResponse => {
 
 const shouldRedirectToDefaultHomepage = (pathname: string): boolean => {
   const defaultHomepage = process.env.DEFAULT_HOMEPAGE;
-  return (pathname === '/' || pathname === '/entando-de-app')
+  return (pathname === '/' || pathname === PORTALUI_BASEPATH || pathname === BASEPATH)
     && defaultHomepage && defaultHomepage.length > 0;
 };
 
 const redirectToHomepage = (): NextResponse => {
-  return NextResponse.redirect(process.env.DEFAULT_HOMEPAGE);
+  return NextResponse.redirect(`${BASEPATH}${process.env.DEFAULT_HOMEPAGE}`);
+};
+
+const shouldRedirectBasePath = (pathname: string): boolean => {
+  return pathname.startsWith(PORTALUI_BASEPATH);
+};
+
+const redirectBasePath = (req: NextRequest): NextResponse => {
+  const old_pathname = req.nextUrl.pathname;
+  const pathname = old_pathname.replace(PORTALUI_BASEPATH, BASEPATH);
+  console.log('Redirecting basepath: ', old_pathname, pathname);
+  return NextResponse.redirect(new URL(pathname, req.url));
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -39,5 +53,17 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
 
   if (shouldRedirectToDefaultHomepage(pathname)) {
     return redirectToHomepage();
+  }
+
+  if (shouldRedirectBasePath(pathname)) {
+    return redirectBasePath(req);
+  }
+
+  if (!pathname.startsWith(BASEPATH)) {
+    return NextResponse.redirect(new URL(`${BASEPATH}/404`, req.url));
+  }
+
+  if (BASEPATH.length > 0 && !pathname.includes('/api/auth')) {
+    return NextResponse.rewrite(pathname.replace(BASEPATH, ''));
   }
 }
