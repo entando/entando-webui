4642013e180db7b66ac26371f7e3a1f6f5fee83b
Filipe Leandro <ffleandro>
2022-03-22 11:23:36 -0300

    Split BASEPATH into API_BASEPATH and APP_BASEPATH to allow a more fine grained configuration;

--- starters/barebone-nextjs-engine/middlewares/basePathRewrite.ts
+++ starters/barebone-nextjs-engine/middlewares/basePathRewrite.ts
@@ -2,19 +2,19 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 const PORTALUI_BASEPATH = new URL(process.env.PORTALUI_ADDR).pathname.replace(/\/$/, '');
-const BASEPATH = `${process.env.BASEPATH}`.replace(/\/$/, '');
+const APP_BASEPATH = `${process.env.APP_BASEPATH}`.replace(/\/$/, '');
 
 export const basepathRewriteMiddleware = (req: NextRequest): NextResponse | undefined => {
   const pathname = req.nextUrl.pathname.replace(/\/$/, '');
   
   if (pathname.startsWith(PORTALUI_BASEPATH)) {
-    const new_pathname = pathname.replace(PORTALUI_BASEPATH, BASEPATH);
+    const new_pathname = pathname.replace(PORTALUI_BASEPATH, APP_BASEPATH);
     return NextResponse.redirect(new URL(new_pathname, req.url));
   }
 
-  if (BASEPATH.length > 0 && !pathname.includes('/api/auth')) {
+  if (APP_BASEPATH.length > 0 && !pathname.includes('/api/auth')) {
     const url = req.nextUrl.clone();
-    url.pathname = pathname.replace(BASEPATH, '');
+    url.pathname = pathname.replace(APP_BASEPATH, '');
     return NextResponse.rewrite(url);
   }
 
--- starters/barebone-nextjs-engine/middlewares/homepageRedirect.ts
+++ starters/barebone-nextjs-engine/middlewares/homepageRedirect.ts
@@ -2,16 +2,16 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 const PORTALUI_BASEPATH = new URL(process.env.PORTALUI_ADDR).pathname.replace(/\/$/, '');
-const BASEPATH = `${process.env.BASEPATH}`.replace(/\/$/, '');
+const APP_BASEPATH = `${process.env.APP_BASEPATH}`.replace(/\/$/, '');
 
 export const homepageRedirectMiddleware = (req: NextRequest): NextResponse | undefined => {
   const pathname = req.nextUrl.pathname.replace(/\/$/, '');
   const defaultHomepage = process.env.DEFAULT_HOMEPAGE;
 
-  if((pathname === '' || pathname === PORTALUI_BASEPATH || pathname === BASEPATH)
+  if((pathname === '' || pathname === PORTALUI_BASEPATH || pathname === APP_BASEPATH)
         && defaultHomepage && defaultHomepage.length > 0) {
     const url = req.nextUrl.clone();
-    url.pathname = `${BASEPATH}${defaultHomepage}`;
+    url.pathname = `${APP_BASEPATH}${defaultHomepage}`;
     return NextResponse.redirect(url);
   }
   
