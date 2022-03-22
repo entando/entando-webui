eb39a30e47e35a05a48eaed8be7dd7d5063bf4f3
Filipe Leandro <ffleandro>
2022-03-22 16:57:11 -0300

    Pushing latest updates on the ci-workspace and the dev-workspace docker images;

--- plugins/ci-workspace-plugin/start.sh
+++ plugins/ci-workspace-plugin/start.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
 
+# ssh-keygen -A
+
 # If there is some public key in keys folder
 # then it copies its contain in authorized_keys file
 if [ "$(ls -A /git-server/keys/)" ]; then
   cd /home/git
-  cat /git-server/keys/*.pub > .ssh/authorized_keys
-  chown -R git:git .ssh
-  chmod 700 .ssh
-  chmod -R 600 .ssh/*
+  cat /git-server/keys/*.pub >> /home/git/.ssh/authorized_keys
+  # chown -R git:git .ssh
+  # chmod 744 .ssh
+  # chmod -R 744 .ssh/*
 fi
 
 # Checking permissions and fixing SGID bit in repos folder
 # More info: https://github.com/jkarlosb/git-server-docker/issues/1
-if [ "$(ls -A /git-server/repos/)" ]; then
-  cd /git-server/repos
-  chown -R git:git .
-  chmod -R ug+rwX .
-  find . -type d -exec chmod g+s '{}' +
-fi
+#if [ "$(ls -A /git-server/repos/)" ]; then
+  # cd /git-server/repos
+  # chown -R git:git .
+  # chmod -R ug+rwX .
+  # find . -type d -exec chmod g+s '{}' +
+#fi
 
 # -D flag avoids executing sshd as a daemon
-/usr/sbin/sshd
+/usr/sbin/sshd -D
+echo "Finished sshd process..."
